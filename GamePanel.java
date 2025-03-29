198a928ba2583b2561e0cba23e53462aa4529db3
UdayBhoyar <124785337+UdayBhoyar@users.noreply.github.com>
2025-03-29 15:42:59 +0530

    Changed the UI of game

--- GamePanel.java
+++ GamePanel.java
@@ -38,7 +38,7 @@ public class GamePanel extends JPanel implements ActionListener {
         direction = 'R';
         running = true;
         newApple();
-        
+
         // Ensure all body parts start from the center to prevent green dot issue
         int startX = SCREEN_WIDTH / 2;
         int startY = SCREEN_HEIGHT / 2;
@@ -46,7 +46,7 @@ public class GamePanel extends JPanel implements ActionListener {
             x[i] = startX - (i * UNIT_SIZE);
             y[i] = startY;
         }
-        
+
         if (timer != null) {
             timer.stop();
         }
@@ -62,24 +62,61 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if (running) {
-            g.setColor(Color.red);
-            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+            // Draw background
+            g.setColor(new Color(50, 50, 50));
+            g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-            for (int i = 0; i < bodyParts; i++) {
-                if (i == 0) {
-                    g.setColor(Color.green);
-                } else {
-                    g.setColor(new Color(45, 180, 0));
-                }
-                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-            }
-            g.setColor(Color.red);
-            g.setFont(new Font("Ink Free", Font.BOLD, 40));
-            FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
+            // Draw apple
+            drawApple(g);
+
+            // Draw snake
+            drawSnake(g);
+
+            // Draw score
+            drawScore(g);
         } else {
             gameOver(g);
         }
+    }
+
+    private void drawApple(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Draw shadow
+        g2d.setColor(new Color(0, 0, 0, 100));
+        g2d.fillOval(appleX + 5, appleY + 5, UNIT_SIZE, UNIT_SIZE);
+
+        // Draw apple with gradient
+        GradientPaint gradient = new GradientPaint(appleX, appleY, Color.red, appleX + UNIT_SIZE, appleY + UNIT_SIZE, Color.orange);
+        g2d.setPaint(gradient);
+        g2d.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+    }
+
+    private void drawSnake(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
+                // Draw head with gradient
+                GradientPaint gradient = new GradientPaint(x[i], y[i], Color.green, x[i] + UNIT_SIZE, y[i] + UNIT_SIZE, new Color(0, 100, 0));
+                g2d.setPaint(gradient);
+                g2d.fillRoundRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE, 10, 10);
+            } else {
+                // Draw body with gradient
+                GradientPaint gradient = new GradientPaint(x[i], y[i], new Color(45, 180, 0), x[i] + UNIT_SIZE, y[i] + UNIT_SIZE, new Color(0, 100, 0));
+                g2d.setPaint(gradient);
+                g2d.fillRoundRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE, 10, 10);
+            }
+        }
+    }
+
+    private void drawScore(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten)) / 2, g.getFont().getSize());
     }
 
     public void newApple() {
